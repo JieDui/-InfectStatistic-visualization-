45ed84c7743efa52ed27f2a142a6c8b546528ba1
YBN <493670735@qq.com>
2020-03-12 21:36:51 +0800

    refactor: prepare for merging code
    prepare for merging code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 import {HeadComponent} from './components/head/head.component';
 import {HomePageModule} from './home/home.module';
+import {HttpClient, HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HomePageModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HomePageModule, HttpClientJsonpModule],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/components/latest-information/latest-information.component.ts
+++ src/app/components/latest-information/latest-information.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-latest-information',
   templateUrl: './latest-information.component.html',
   styleUrls: ['./latest-information.component.scss'],
 })
+
 export class LatestInformationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    // this.http.get('assets/articles.json')
+    //     .pipe(data => data.json())
+    //     .subscribe(data => {
+    //       console.log(data);
+    //     });
+
+  }
 
   ngOnInit() {}
 
